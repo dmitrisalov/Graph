95ab8ef0fbc4d84a76170202980a38c4ca6aa005
Dmitri Salov <dmitrisalov98@gmail.com>
2017-12-28 18:16:01 -0600

    Added testing of edge constructors.

--- test.cpp
+++ test.cpp
@@ -1,1 +1,21 @@
 #include "Graph.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	Edge edge1 = new Edge();
+	Edge edge2 = new Edge(0, 1, 0);
+	
+	vector<Edge> edges;
+	edges.push_back(edge1);
+	edges.push_back(edge2);
+	
+	for (Edge edge : edges) {
+		cout << "Start: " << edge.source << " End: " << edge.end;
+		cout << " Weight: " << edge.weight << endl;
+	}
+	
+	return 0;
+}
